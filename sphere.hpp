62f5c6231cf3651d1f93b4b3684d16362c8ba32d
Yuki Sakamoto <sakamotoyuki.jpn@gmail.com>
2021-02-14 22:11:55 +0900

    add 5.5

--- sphere.hpp
+++ sphere.hpp
@@ -16,12 +16,12 @@ public:
 bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const 
 {
     vec3 oc = r.origin() - center;
-    float a = dot(r.direction, r.direction());
+    float a = dot(r.direction(), r.direction());
     float b = dot(oc, r.direction());
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - a*c;
     if (discriminant > 0) {
-        float temp = (0b - sqrt(discriminant)) / a;
+        float temp = (-b - sqrt(discriminant)) / a;
         if (temp < t_max && temp > t_min) {
             rec.t = temp;
             rec.p = r.point_at_parameter(rec.t);
